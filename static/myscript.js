ca13f2b9897496b26f48730d4e3169beb187bf85
Leslie Castellanos <leslie@Leslies-MacBook-Air.local>
2015-11-10 19:53:43 -0800

    Google sign in and token authentication.

--- static/myscript.js
+++ static/myscript.js
@@ -1,13 +1,13 @@
 $( document ).ready(function() {
 
-    function onSignIn(googleUser) {
-      var profile = googleUser.getBasicProfile();
-      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-      console.log('Name: ' + profile.getName());
-      console.log('Image URL: ' + profile.getImageUrl());
-      console.log('Email: ' + profile.getEmail());
-    }
+    document.getElementById("signout").addEventListener("click", signOut);
 
+    function signOut() {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+          console.log('User signed out.');
+        });
+      }
     var myPoints = 0;
     var next = 1;
     var completed = 0;
